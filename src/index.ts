0c092bab9d3829184ad9ff237400ffbbd066f3de
Roberto <114288685+SaturnCassini@users.noreply.github.com>
2023-01-03 13:33:07 +0100

    adding the ring and moons orbiting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { PerspectiveCamera, Scene, WebGLRenderer, SphereGeometry, Mesh, MeshNormalMaterial } from 'three';
+import * as THREE from 'three'
+
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { Vector3 } from 'three';
@@ -6,18 +8,83 @@ import { Point3 } from './util/types';
 import { PlasmaOrbMaterial } from './shaders/plasma-orb/plasma-orb-filter';
 import { PlasmaOrb } from './objects/plasma-orb';
 import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+
+
+const path = './objects/SSTITLE.gltf';
 
 
 
 const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 50);
-camera.position.z = 20;
+camera.position.z = 3;
+camera.position.y = 0;
 
 const scene = new Scene();
 
-const sphere = buildSphere(10, { x: 0, y: 0, z: -20 }, scene);
+// Create Saturn
+const saturn = new PlasmaOrb(1);
+scene.add(saturn);
 
-//const orb = new PlasmaOrb(10);
-//scene.add(orb);
+// Create Rings
+const ringGeometry = new THREE.RingGeometry(1.2, 2, 50)
+const ringMaterial = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide})
+const ring = new THREE.Mesh(ringGeometry, ringMaterial)
+ring.rotation.x =  1.7
+const ringSystem = new THREE.Object3D()
+ringSystem.add(ring)
+saturn.add(ringSystem)
+camera.lookAt(saturn.position)
+
+// Add orbiting moons
+// Titan
+const titanGeometry = new THREE.SphereGeometry(0.1, 32, 32)
+const titanMaterial = new THREE.MeshNormalMaterial()
+const titan = new THREE.Mesh(titanGeometry, titanMaterial)
+titan.position.set(3, 0, 0)
+const titanSystem = new THREE.Object3D()
+titanSystem.add(titan)
+saturn.add(titanSystem)
+// Encelaedus
+const encelaedusGeometry = new THREE.SphereGeometry(0.4, 32, 32)
+const encelaedusMaterial = new THREE.MeshNormalMaterial()
+const encelaedus = new THREE.Mesh(encelaedusGeometry, encelaedusMaterial)
+encelaedus.position.set(6, 0, 0)
+const encelaedusSystem = new THREE.Object3D()
+encelaedusSystem.add(encelaedus)
+saturn.add(encelaedusSystem)
+// Mimas
+const mimasGeometry = new THREE.SphereGeometry(0.2, 32, 32)
+const mimasMaterial = new THREE.MeshNormalMaterial()
+const mimas = new THREE.Mesh(mimasGeometry, mimasMaterial)
+mimas.position.set(8, 0, 0)
+const mimasSystem = new THREE.Object3D()
+mimasSystem.add(mimas)
+saturn.add(mimasSystem)
+// Tethys
+const tethysGeometry = new THREE.SphereGeometry(0.3, 32, 32)
+const tethysMaterial = new THREE.MeshNormalMaterial()
+const tethys = new THREE.Mesh(tethysGeometry, tethysMaterial)
+tethys.position.set(10, 0, 0)
+const tethysSystem = new THREE.Object3D()
+tethysSystem.add(tethys)
+saturn.add(tethysSystem)
+// Dione
+const dioneGeometry = new THREE.SphereGeometry(0.2, 32, 32)
+const dioneMaterial = new THREE.MeshNormalMaterial()
+const dione = new THREE.Mesh(dioneGeometry, dioneMaterial)
+dione.position.set(12, 0, 0)
+const dioneSystem = new THREE.Object3D()
+dioneSystem.add(dione)
+saturn.add(dioneSystem)
+// Rhea
+const rheaGeometry = new THREE.SphereGeometry(0.2, 32, 32)
+const rheaMaterial = new THREE.MeshNormalMaterial()
+const rhea = new THREE.Mesh(rheaGeometry, rheaMaterial)
+rhea.position.set(14, 0, 0)
+const rheaSystem = new THREE.Object3D()
+rheaSystem.add(rhea)
+saturn.add(rheaSystem)
 
 
 
@@ -34,20 +101,66 @@ window.addEventListener('resize', onResize, false);
 const composer = new EffectComposer(renderer);
 composer.addPass(new RenderPass(scene, camera));
 
-const effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: true });
+const effect = new AsciiEffect(renderer, ' .-+*%@', { invert: true });
 effect.setSize(window.innerWidth, window.innerHeight);
-effect.domElement.style.color = 'white';
+effect.domElement.style.color = 'lightgreen';
 effect.domElement.style.backgroundColor = 'black';
-
 document.body.appendChild(effect.domElement);
+
+// Add Orbit Controls
+// Controls
+const controls = new OrbitControls(camera, effect.domElement)
+controls.enableDamping = true
+controls.dampingFactor = 0.05
+controls.maxDistance = 15
+controls.minDistance = 0.1
+
+// // Add a title  to the scene
+// // create canvas element and draw text on it
+// const canvas = document.createElement('canvas');
+// const ctx = canvas.getContext('2d');
+// ctx.font = 'bold 36px Arial';
+// ctx.fillStyle = 'white';
+// ctx.fillText('SATURN SERIES', 0, 36);
+
+// // create texture from canvas element
+// const texture = new THREE.CanvasTexture(canvas);
+
+// // create plane and apply texture to it
+// const geometry = new THREE.PlaneGeometry(canvas.width, canvas.height);
+// const material = new THREE.MeshNormalMaterial({ map: texture });
+// const plane = new THREE.Mesh(geometry, material);
+
+// // add plane to scene
+// scene.add(plane);
+
+// // First, create a text geometry with the desired message and font
+// const textGeometry = new TextGeometry("SATURN SERIES", {
+//     font: font, // font is an instance of THREE.Font
+//     size: 10,
+//     height: 2
+//   });
+  
+//   // Then, create a material for the text
+//   const textMaterial = new THREE.MeshNormalMaterial({ });
+  
+//   // Finally, create the text mesh and add it to the scene
+//   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+//   scene.add(textMesh);
+
 function update(dt: number) {
 
-    //orb.update(dt);
-    //composer.render(dt);
+    saturn.update(dt);
+    composer.render(dt);
+    titanSystem.rotation.y += 0.02
+    encelaedusSystem.rotation.y += 0.01
+    mimasSystem.rotation.y += 0.03
+    tethysSystem.rotation.y += 0.02
+    dioneSystem.rotation.y += 0.01
+    rheaSystem.rotation.y += 0.03
+    ringSystem.rotation.y += 0.01
+    scene.rotation.y += 0.01
     effect.render(scene, camera);
-    //renderer.render(scene, camera)
-
-
 }
 
 function onResize() {
@@ -56,6 +169,7 @@ function onResize() {
 
     composer.setSize(window.innerWidth, window.innerHeight);
 }
+
 
 
 function buildSphere(radius: number, at?: Point3, scene?: Scene) {
@@ -72,3 +186,12 @@ function buildSphere(radius: number, at?: Point3, scene?: Scene) {
     return mesh;
 
 }
+
+// function animate() {
+//     requestAnimationFrame(animate);
+//     sphere.rotation.x += 0.01;
+
+//     sphere.rotation.y += 0.01;
+//     render()
+// }
+// animate();
